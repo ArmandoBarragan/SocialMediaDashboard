f806fd59d07b87cbc1f93f70f12177c6d9ab8bef
ArmandoBarragan <armando.barragan2@outlook.com>
2021-03-14 17:39:58 -0700

    The login works and lets know the login or usr creation errors

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -7,8 +7,3 @@ class UserForm(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ('business_name', 'email')
 
-class LoginForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = User
-        fields = ('username', 'password')
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,45 +1,54 @@
 from django.shortcuts import render, redirect, reverse
-from dashboard.forms import UserForm, LoginForm
+from dashboard.forms import UserForm
 from django.contrib.auth import logout as do_logout, login as do_login, authenticate
 from dashboard.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 # Create your views here.
+
+
 def index_view(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return render(request, 'index.html')
     return redirect('login')
 
+
 def login_view(request):
-    context = {'form': LoginForm}
+    context = {'form': AuthenticationForm}
+
     if request.method == 'POST':
-        form = LoginForm(data=request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
             user = authenticate(username=username, password=password)
-
             if user is not None:
                 do_login(request, user)
                 return redirect('index')
             else:
-                print('something went wrong')
-            
-            
+                context = {'form': form}
+                if form.errors:
+                    print(form.errors)
+
     return render(request, 'authentication/login.html', context)
-    
+
 
 def creation_view(request):
+    context = {'form': UserForm}
     if request.method == "POST":
         form = UserForm(data=request.POST)
-        
+
         if form.is_valid():
             user = form.save()
-            login(request, user) 
+            do_login(request, user)
             return redirect(reverse(index_view))
-        
-    context = {'form': UserForm}
+        else:
+            context = {'form': form}
+
     return render(request, 'authentication/create.html', context)
+
 
 def logout(request):
     do_logout(request)
